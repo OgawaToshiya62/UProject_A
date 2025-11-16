bf505c82b66e02f1ea2ac6067739bd048e85cc87
OgawaToshiya62 <game22403013@gmail.com>
2025-11-16 14:47:27 +0900

    Add Anim
    Section2-15

---
+++ Source/Warrior/Private/AnimInstances/WarriorCharacterAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimInstances/WarriorCharacterAnimInstance.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UWarriorCharacterAnimInstance::NativeInitializeAnimation()
+{
+	OwningCharacter = Cast<AWarriorBaseCharacter>(TryGetPawnOwner());     // TryGetPawnOwner アニメーションが属しているキャラクターを取得する関数 Cast<AWarriorBaseCharacter>(...) TryGetPawnOwnerはAPawnを返すがAWarriorBaseCharacter型として扱いたいため使用 Castで安全に型変換する(失敗するとnull)
+	
+	// キャラクターが存在する場合、その移動コンポーネントを取得して保存
+	if (OwningCharacter)
+	{
+		OwningMovementComponent = OwningCharacter -> GetCharacterMovement();
+	}
+}
+
+void UWarriorCharacterAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	// キャラクターまたは移動コンポーネントが無効な場合は処理を中断
+	if (!OwningCharacter || !OwningMovementComponent)
+	{
+		return;
+	}
+
+	//地面上の移動速度を取得（アニメーション用）
+	GroundSpeed = OwningCharacter -> GetVelocity().Size2D();
+
+	// 移動入力があるかどうかを判定（加速度があるか）
+	bHasAcceleration = OwningMovementComponent -> GetCurrentAcceleration().SizeSquared2D() > 0.f;
+}
---
+++ Source/Warrior/Public/AnimInstances/WarriorCharacterAnimInstance.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimInstances/WarriorBaseAnimInstance.h"
+#include "WarriorCharacterAnimInstance.generated.h"
+
+class AWarriorBaseCharacter;
+class UCharacterMovementComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorCharacterAnimInstance : public UWarriorBaseAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;                    // アニメーションインスタンスが初めて使われるときに呼ばれる関数　キャラクター参照、取得をしたり初期状態に設定する
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);     // 毎フレーム呼ばれる更新関数　通常のNativeUpdateAnimationと違いスレッドセーフ(並列処理に安全)な更新処理を行うための関数　複雑なロジックや状態更新をここに書くことでアニメーションの滑らかさや安全性を保つことができる
+
+protected:
+	UPROPERTY()                                 // キャラクター参照（前方宣言＋空のUPROPERTYで依存を最小化）
+	AWarriorBaseCharacter* OwningCharacter;     // TryGetPawnOwner() で取得したキャラクターを AWarriorBaseCharacter にキャストして保持する
+
+	// キャラクターの移動状態（速度・加速度など）を取得するための MovementComponent の参照
+	UPROPERTY()
+	UCharacterMovementComponent* OwningMovementComponent;
+
+	// 地面上の移動速度（XY平面の速さ）。アニメーション制御に使用
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float GroundSpeed;
+
+	// キャラクターが加速中かどうか（移動入力があるか）を示すフラグ
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bHasAcceleration;
+};
