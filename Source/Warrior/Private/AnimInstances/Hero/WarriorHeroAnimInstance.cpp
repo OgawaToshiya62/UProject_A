ba6b224da3f2df72479c8aa3559b98362e185a2a
OgawaToshiya62 <game22403013@gmail.com>
2025-11-19 16:56:01 +0900

    Update Anim
    Section 2-17

--- Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -2,4 +2,34 @@
 
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
+#include "Characters/WarriorHeroCharacter.h"
 
+// アニメーション初期化時にキャラクターの参照を取得し、特定の型にキャストして保存
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+// キャラクターが動いている場合はアイドル時間をリセットし、リラックス状態を解除
+// 動いていない場合はアイドル時間を加算し、閾値を超えたらリラックス状態に入る
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElpasedTime += DeltaSeconds;
+
+		bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxtStateThreshold);
+	}
+}
--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,8 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
+
 /**
  * 
  */
@@ -14,4 +16,23 @@ class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstanc
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override;                    // アニメーション初期化時に呼ばれる関数（キャラクター参照などを取得）
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);     // 毎フレーム呼ばれるスレッドセーフなアニメーション更新関数
+
+protected:
+	// アニメーションの持ち主キャラクター（Blueprintから読み取り可能）
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|Refrences")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+
+	// 一定時間動きがなければリラックス状態に入るかどうかのフラグ
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	// リラックス状態に入るまでの待機時間（秒）
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxtStateThreshold = 5.f;
+
+	// キャラクターが動いていない時間の累積（リラックス判定用）
+	float IdleElpasedTime;
 };
