d8e7e0bb7ed2273b52d7d2efd6465778a483207a
OgawaToshiya62 <game22403013@gmail.com>
2025-11-23 12:54:09 +0900

    Add AbilitySystemComponent
    Section 2-19

---
+++ Source/Warrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "WarriorGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class EWarriorAbilityActivationPolicy : uint8
+{
+	OnTriggered,     // プレイヤー操作などでトリガーされたときに発動
+	OnGiven          // アビリティが付与された瞬間に自動発動
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	//~ Begin UGameplayAbility Interface.
+	// アビリティがキャラクターに付与されたときに呼ばれる処理（初期化など）
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec);
+	// アビリティが終了するときに呼ばれる処理（キャンセルや終了時の後処理
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+	//~ End UGameplayAbility Interface
+
+	// アビリティの発動タイミングを決める（トリガー時か付与時か）
+	UPROPERTY(EditDefaultsOnly, Category = "WarriorAbility")
+	EWarriorAbilityActivationPolicy AbilityActivationPolicy = EWarriorAbilityActivationPolicy::OnTriggered;
+};
