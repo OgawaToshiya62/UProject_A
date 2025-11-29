943ab4eaf969a03f7bbed75fdfa9eee44edffd61
OgawaToshiya62 <game22403013@gmail.com>
2025-11-29 12:59:40 +0900

    Add Start Up Data
    Section 2-21
    39

---
+++ Source/Warrior/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_StartUpDataBase.generated.h"
+
+class UWarriorGameplayAbility;
+class UWarriorAbilitySystemComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UDataAsset_StartUpDataBase : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// 指定された AbilitySystemComponent にアビリティを付与する
+	virtual void GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel = 1);
+	
+protected:
+	// 付与された瞬間に自動発動するアビリティのリスト
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+	TArray< TSubclassOf < UWarriorGameplayAbility > > ActivateOnGivenAbilities;
+
+	// 条件やイベントに反応して発動するアビリティのリスト
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+	TArray< TSubclassOf < UWarriorGameplayAbility > > ReactiveAbilities;
+
+	// 指定されたアビリティ群を AbilitySystemComponent に付与する処理
+	void GrantAbilities(const TArray< TSubclassOf < UWarriorGameplayAbility > >& InAbilitiesToGive, UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel = 1);
+};
