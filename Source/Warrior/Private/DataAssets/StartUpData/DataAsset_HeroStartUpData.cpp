1ddb1ea8a7927dc2421b407d64e67c34eb043bc2
OgawaToshiya62 <game22403013@gmail.com>
2025-12-07 21:22:41 +0900

    Hero Ability Set
    Section3-32
    60

--- Source/Warrior/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/Warrior/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -2,4 +2,30 @@
 
 
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 
+// 入力タグとアビリティクラスが揃っているかを判定する
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+// ヒーロー開始時のアビリティセットを AbilitySystemComponent に付与する
+// 有効なセットごとに AbilitySpec を作成し、入力タグやレベルを設定して渡す
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	for (const FWarriorHeroAbilitySet& AbilitySet : HeroStartUpAbilitySets)
+	{
+		if (!AbilitySet.IsValid())continue;
+
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}
--- Source/Warrior/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
+++ Source/Warrior/Public/DataAssets/StartUpData/DataAsset_HeroStartUpData.h
@@ -4,8 +4,26 @@
 
 #include "CoreMinimal.h"
 #include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_HeroStartUpData.generated.h"
 
+USTRUCT(BlueprintType)
+// ヒーローが持つアビリティセット（入力タグと付与するアビリティの組み合わせ）
+struct FWarriorHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	// このアビリティを呼び出すための入力タグ
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	// 付与するアビリティのクラス
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UWarriorGameplayAbility> AbilityToGrant;
+
+	// 入力タグとアビリティが有効かどうかを判定する
+	bool IsValid() const;
+};
 /**
  * 
  */
@@ -13,5 +31,13 @@ UCLASS()
 class WARRIOR_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataBase
 {
 	GENERATED_BODY()
+
+public:
+	// 指定した AbilitySystemComponent に、このヒーローのアビリティを付与する
+	virtual void GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel = 1) override;
 	
+private:
+	// ゲーム開始時に付与するヒーロー用アビリティセット一覧
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorHeroAbilitySet> HeroStartUpAbilitySets;
 };
