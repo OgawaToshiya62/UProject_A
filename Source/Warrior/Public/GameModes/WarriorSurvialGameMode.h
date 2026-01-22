67a3bcdb936f65c795b1d0f750519b29da1bf8ad
OgawaToshiya62 <game22403013@gmail.com>
2026-01-22 23:20:04 +0900

    Revert "Update GameMode"
    This reverts commit 30be0bff64a58d3b71db18565af0c68bd62e81da.

--- Source/Warrior/Public/GameModes/WarriorSurvialGameMode.h
+++ Source/Warrior/Public/GameModes/WarriorSurvialGameMode.h
@@ -6,8 +6,6 @@
 #include "GameModes/WarriorBaseGameMode.h"
 #include "WarriorSurvialGameMode.generated.h"
 
-class AWarriorEnemyCharacter;
-
 // サバイバルゲームモードの進行状態を表す列挙体
 UENUM(BlueprintType)
 enum class EWarriorSurvialGameModeState : uint8
@@ -18,40 +16,6 @@ enum class EWarriorSurvialGameModeState : uint8
 	WaveCompleted,
 	AllWavesDone,
 	PlayerDied
-};
-
-// 1つのスポーン定義（どの敵を何体スポーンさせるか）を表す構造体
-USTRUCT(BlueprintType)
-struct FWarriorEnemyWaveSpawnerInfo
-{
-	GENERATED_BODY()
-
-	// スポーンする敵キャラクターのクラス
-	UPROPERTY(EditAnywhere)
-	TSoftClassPtr<AWarriorEnemyCharacter> SoftEnemyClassToSpawn;
-
-	// 1回のスポーンで最低何体出すか
-	UPROPERTY(EditAnywhere)
-	int32 MinPerSpawnCount = 1;
-
-	// 1回のスポーンで最大何体出すか
-	UPROPERTY(EditAnywhere)
-	int32 MaxPerSpawnCount = 3;
-};
-
-// データテーブルの1行を表す構造体
-USTRUCT(BlueprintType)
-struct FWarriorEnemyWaveSpawnerTableRow : public FTableRowBase
-{
-	GENERATED_BODY()
-
-	// この Wave で使用するスポーン定義のリスト
-	UPROPERTY(EditAnywhere)
-	TArray<FWarriorEnemyWaveSpawnerInfo> EnemyWaveSpawnerDefinitions;
-
-	// この Wave 全体でスポーンする敵の総数
-	UPROPERTY(EditAnywhere)
-	int32 TotalEnemyToSpawnThisWave = 1;
 };
 
 // ゲームモードの状態が変化したときに通知するデリゲート
@@ -74,8 +38,6 @@ protected:
 private:
 	// 現在のゲームモード状態を変更し、Blueprint に通知する
 	void SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState InState);
-	// 全ての Wave を終えたかどうかを判定する
-	bool HasFinishedAllWaves() const;
 
 	// 現在のサバイバルゲームモードの状態
 	UPROPERTY()
@@ -84,32 +46,4 @@ private:
 	// 状態変更を Blueprint に通知するデリゲート
 	UPROPERTY(BlueprintAssignable, BlueprintCallable)
 	FOnSurvialGameModeStateChangedDelegate OnSurvialGameModeStateChanged;
-
-	// Wave の定義を格納したデータテーブル
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
-	UDataTable* EnemyWaveSpawnerDataTable;
-
-	// データテーブルに定義された Wave の総数
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
-	int32 TotalWavesToSpawn;
-
-	// 現在の Wave数
-	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
-	int32 CurrentWaveCount = 1;
-
-	// 状態遷移のために経過時間を蓄積するタイマー
-	UPROPERTY()
-	float TimePassedSinceStart = 0.f;
-
-	// 次の Wave をスポーンするまでの待機時間
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
-	float SpawnNewWaveWaitTime = 5.f;
-
-	// Wave スポーン開始後、敵を出現させるまでの遅延時間
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
-	float SpawnEnemiesDelayTime = 2.f;
-
-	// Wave 完了後、次の Wave に移行するまでの待機時間
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "WaveDefinition", meta = (AllowPrivateAccess = "true"))
-	float WaveCompletedWaitTime = 5.f;
 };
