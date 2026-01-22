30be0bff64a58d3b71db18565af0c68bd62e81da
OgawaToshiya62 <game22403013@gmail.com>
2026-01-22 17:10:27 +0900

    Update GameMode
    Section 10-275, 276, 277, 278
    30 32 14 7

--- Source/Warrior/Private/GameModes/WarriorSurvialGameMode.cpp
+++ Source/Warrior/Private/GameModes/WarriorSurvialGameMode.cpp
@@ -7,12 +7,68 @@
 void AWarriorSurvialGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	checkf(EnemyWaveSpawnerDataTable, TEXT("Forgot to assign a valid datat table in survial game mode blueprint"));
+
+	SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState::WaitSpawnNewWave);
+
+	TotalWavesToSpawn = EnemyWaveSpawnerDataTable -> GetRowNames().Num();
 }
 
 // 毎フレーム呼ばれる
 void AWarriorSurvialGameMode::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	// --- 次の Wave をスポーンするまで待機 ---
+	if (CurrentSurvialGameModeState == EWarriorSurvialGameModeState::WaitSpawnNewWave)
+	{
+		TimePassedSinceStart += DeltaTime;
+
+		if (TimePassedSinceStart >= SpawnNewWaveWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+
+			SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState::SpawningNewWave);
+		}
+	}
+
+	// --- Wave スポーン中（敵を出す前の遅延）---
+	if (CurrentSurvialGameModeState == EWarriorSurvialGameModeState::SpawningNewWave)
+	{
+		TimePassedSinceStart += DeltaTime;
+
+		if (TimePassedSinceStart >= SpawnEnemiesDelayTime)
+		{
+			// TODO : Handle Spawn New Enemies
+
+			TimePassedSinceStart = 0.f;
+
+			SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState::InProgress);
+		}
+	}
+
+	// --- Wave 完了後の待機 ---
+	if (CurrentSurvialGameModeState == EWarriorSurvialGameModeState::WaveCompleted)
+	{
+		TimePassedSinceStart += DeltaTime;
+
+		if (TimePassedSinceStart >= WaveCompletedWaitTime)
+		{
+			TimePassedSinceStart = 0.f;
+
+			CurrentWaveCount++;
+
+			if (HasFinishedAllWaves())
+			{
+				SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState::AllWavesDone);
+			}
+			else
+			{
+				SetCurrentSurvialGameModeState(EWarriorSurvialGameModeState::WaitSpawnNewWave);
+			}
+		}
+	}
 }
 
 // ゲームモードの状態を変更し、Blueprint に通知する
@@ -22,4 +78,10 @@ void AWarriorSurvialGameMode::SetCurrentSurvialGameModeState(EWarriorSurvialGame
 
 	// 状態変更を Blueprint に通知
 	OnSurvialGameModeStateChanged.Broadcast(CurrentSurvialGameModeState);
+}
+
+// 全ての Wave を終えたかどうかを判定する
+bool AWarriorSurvialGameMode::HasFinishedAllWaves() const
+{
+	return CurrentWaveCount >= TotalWavesToSpawn;
 }
